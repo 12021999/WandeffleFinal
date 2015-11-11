61e745c3288d4214090f5db9b60a47c1aabcb51b
Matheus Oliveira <erick.oliveira@NAVE035-7EIR0CQ.nave.local>
2015-11-11 10:44:43 -0200

    Final version
    -all done
    -need repairs

---
+++ Wandeffle 0.7/Assets/Scripts/HighScore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+	public GameObject highScore;
+	private string link;
+	void Start()
+	{
+		Network.proxyIP = "10.10.10.1";
+		Network.proxyPort = 3128;
+		Network.useProxy = true;
+		link = "http://jamalnave1.16mb.com/index.php";
+		StartCoroutine(GetHighScore(link));
+	}
+	
+	IEnumerator GetHighScore(string url)
+	{
+		WWW jamal = new WWW(url);
+		new WaitForSeconds(1f);
+		yield return jamal;
+		Debug.Log (jamal.text);
+		highScore.GetComponent<Text>().text = "HighScore : " + jamal.text;
+	}
+}
---
+++ Wandeffle 0.7/Assets/Scripts/ScoreGameover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using UnityEngine.UI;
+
+public class ScoreGameover : MonoBehaviour {
+
+	public GameObject Scoregameover;
+	private string url;
+	
+	void Start () 
+	{
+		url = "http://jamalnave1.16mb.com/Conecta.php";
+		SetScoreToData();
+	}
+	
+	void Update () 
+	{
+		int scoregameover = PlayerPrefs.GetInt("Player Score");
+		Scoregameover.GetComponent<Text>().text = "Score : " + scoregameover.ToString();
+	}
+	void SetScoreToData()
+	{
+		WWWForm rada = new WWWForm();
+		rada.AddField("score",PlayerPrefs.GetInt("Player Score"));
+		
+		WWW www = new WWW(url,rada);
+	}
+}
